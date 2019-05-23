3a11cfbdeb041caaae5b610ba1c2fdc0d57173f2
Joy James <joyjames@techie.com>
2019-05-23 19:07:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+question = input("Would you like a cup of tee? ").lower()
+
+if question == "yes":
+    print("I will come back with it now")
+else:
+    print("Let me know if you change your mind")
